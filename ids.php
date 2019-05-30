31f4915a72c22c89fe903fb727b1d71f3bacc26f
Biplob SD <biplobsd11@gmail.com>
2019-05-31 01:14:42 +0600

    First commit

---
+++ ids.php
@@ -0,0 +1,19 @@
+<?php 
+// // Which id to send all coin //
+// // Remove "//" before start this script //
+
+//$sendToM = 'xxxxx@gmail.com';
+
+// // Ids lists //
+// $keyV = array(
+    // 0 => array(
+        // "key" => " ",
+        // "secret" => " ",
+    // ),
+    // 1 => array(
+        // "key" => " ",
+        // "secret" => " ",
+    // ),
+// );
+
+?>
